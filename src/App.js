0eb18a7bfa709a0dbb8dcf479570407ee2f77fba
ZhelinWang <zhelinwang0122@gmail.com>
2022-06-19 00:16:05 +1200

    dropdown1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import {  useState  } from "react";
+import Dropdown from "./DropdownTile"
 
 function App() {
+  const[selected, setSelected] = useState("Select Type of Tile ")
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+    <Dropdown selected={selected} setSelected={setSelected} />
+    <Dropdown selected={selected} setSelected={setSelected} />
     </div>
+    
   );
-}
+} 
+
 
 export default App;
