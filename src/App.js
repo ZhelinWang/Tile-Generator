9258d34318e3e5ea6a55b7719d9c37b4c2317d7d
ZhelinWang <zhelinwang0122@gmail.com>
2022-06-20 02:17:06 +1200

    Massive Update

--- src/App.js
+++ src/App.js
@@ -1,20 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
-import {  useState  } from "react";
-import DropdownTile from "./DropdownTile"
-import DropdownDeal from "./DropdownDeal"
+import logo from './logo.svg'
+import './App.css'
+import { useState } from 'react'
+import Dropdown from './components/dropdown-menu/Dropdown'
+import Button from '@mui/material/Button';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
 
-function App() {
-  const[selectedTile, setSelectedTile] = useState("Select Type of Tile ")
-  const[selectedDeal, setSelectedDeal] = useState("Select Type of Tile ")
+function App () {
+  const [selectedTile, setSelectedTile] = useState('Select Type of Tile ')
+  const [selectedDeal, setSelectedDeal] = useState('Select Type of Deal ')
+
   return (
     <div className='App'>
-    <DropdownTile style={{zIndex: '1'}} selected={selectedTile} setSelected={setSelectedTile} />
-    <DropdownDeal style={{zIndex: '10'}} selected={selectedDeal} setSelected={setSelectedDeal} />
+      <img className='t7Logo' src={logo} alt="T7 Logo" />
+      <Dropdown
+        style={{ zIndex: '10' }}
+        options={['Large Tile', 'Small Tile', 'Banner']}
+        selected={selectedDeal}
+        setSelected={setSelectedDeal}
+      />
+      <div>
+        <input className='inputFields' placeholder='Input Tile Title Here...' />
+      </div>
+      <div>
+        <input
+          className='inputFields'
+          placeholder='Input Tile Image URL Here...'
+        />
+      </div>
+      <div>
+        <input
+          className='inputFields'
+          placeholder='Input Tile Picklist URL Here...'
+        />
+      </div>
+      <div>
+        <input
+          className='inputFields'
+          placeholder='Input Tile *Excludes Here...'
+        />
+      </div>
+      <Dropdown
+        style={{ zIndex: '1' }}
+        options={["Item X% Off", "Item X up to X% Off", "Buy X get X% Off", "Buy X get X Free/Half Price", "Buy X for X"]}
+        selected={selectedTile}
+        setSelected={setSelectedTile}
+      />
+      <div>
+        <Button className="generateButton" color="error" variant="contained">Generate</Button>
+        <FormControlLabel labelPlacement="start" className='checkboxClub' control={<Checkbox sx={{ '& .MuiSvgIcon-root': { fontSize: 30 } }}/>} label="Club?" />
+        <FormControlLabel labelPlacement="start" className='checkboxClub' control={<Checkbox sx={{ '& .MuiSvgIcon-root': { fontSize: 30 } }}/>} label="Minified?" />
+      </div>
     </div>
-    
-  );
-} 
+  )
+}
 
-
-export default App;
+export default App
--- src/components/dropdown-menu/Dropdown.jsx
+++ src/components/dropdown-menu/Dropdown.jsx
@@ -1,7 +1,8 @@
 import { useState } from 'react'
-function DropdownDeal ({selected, setSelected}) {
+
+function DropdownDeal ({options, selected, setSelected}) {
   const [isActive, setIsActive] = useState(false);
-  const options = ["Item X% Off", "Item X up to X% Off", "Buy X get X% Off", "Buy X get X Free/Half Price", "Buy X for X"];
+
   return (
     <div className='dropdown'>
       <div className='dropdown-btn' onClick={(e) => setIsActive(!isActive)}>
