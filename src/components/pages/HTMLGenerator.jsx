84e498a6462f19fcab482b8efb55c87b041544d5
ZhelinWang <zhelinwang0122@gmail.com>
2023-02-01 14:44:52 +1300

    whites

--- src/components/pages/HTMLGenerator.jsx
+++ src/components/pages/HTMLGenerator.jsx
@@ -577,7 +577,7 @@ function HTMLGenerator (props) {
       <GenerateButton onButtonClick={handleGenerate} />
       <button className='copyButton'
         onClick={() =>  
-        navigator.clipboard.writeText(output) && alert('Output Copied to Clipboard')
+        navigator.clipboard.writeText(output.replace("\\s", "")) && alert('Output Copied to Clipboard')
         }
       >
         Copy Output
